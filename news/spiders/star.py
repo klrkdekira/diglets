d0a2921b3c406c227a68bdd05167577d263bafc9
cheeleong <klrkdekira@gmail.com>
2013-01-05 15:43:03 +0800

    Added definition for Star

--- news/spiders/star.py
+++ news/spiders/star.py
@@ -4,7 +4,19 @@ from news.items import NewsItem
 class StarSpider(XMLFeedSpider):
     name = 'star'
     allowed_domains = ['thestar.com']
-    start_urls = ['http://www.thestar.com/feed.xml']
+    start_urls = ['http://thestar.com.my/rss/nation.xml',
+                  'http://thestar.com.my/rss/sarawak.xml',
+                  'http://thestar.com.my/rss/business.xml',
+                  'http://thestar.com.my/rss/sports.xml',
+                  'http://football.thestar.com.my/category/news/feed',
+                  'http://thestar.com.my/rss/worldupdates.xml',
+                  'http://thestar.com.my/rss/columnists.xml',
+                  'http://thestar.com.my/rss/opinion.xml',
+                  'http://thestar.com.my/rss/central.xml',
+                  'http://thestar.com.my/rss/north.xml',
+                  'http://thestar.com.my/rss/perak.xml',
+                  'http://thestar.com.my/rss/southneast.xml',
+                  'http://thestar.com.my/rss/sundaymetro.xml']
     iterator = 'iternodes' # you can change this; see the docs
     itertag = 'item' # change it accordingly
 
